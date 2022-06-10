b17d8d886285a4d55e62f4db3aaa24f6874a916b
Administrator <valeraryan@gmail.com>
2022-06-10 12:28:19 -0700

    Updated display function to display numbers as they are pressed

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let numList = [0];
+
 function add(num1, num2) {
     return (num1 + num2);
 };
@@ -30,17 +32,33 @@ function operate(data) {
 }
 
 
-let numList = []
+function updateDisplay(displayList) {
+    let number = displayList.join('');
+    console.log(number);
+    let displayOne = document.querySelector('h2');
+    displayOne.innerText = number;
+    if(displayList.length > 9) {
+        display.innerText = number.substring(0, 9);
+    }
+}
+
+updateDisplay([0]);
+
 let displayOne = document.querySelector('h5');
 console.log(displayOne.innerText);
 let calc = document.querySelector('.calculator');
 calc.addEventListener('click', function(e) {
 
-    let digit = e.target.innerText;
-    numList.push(digit);
-    let display = document.querySelector('.display');
+    let digit = e.target.id;
+    console.log(digit);
+    if (['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(digit)) {
+        numList.push(digit);
+        updateDisplay(numList);
+    } else if (digit === 'add') {
+        alert('hello');
+    }
 
-});
+})
 
 // console.log(operate(['/',10,2]));
 
